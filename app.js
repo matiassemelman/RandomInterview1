7a8f7e7b87c2a7c3feb546a730bd8673936ae6f8
matiassemelman <matiassemelman@gmail.com>
2021-12-12 23:33:50 -0300

    deleted and starting again

--- app.js
+++ app.js
@@ -1,48 +1,7 @@
-let form = document.querySelector("form");
-let messagesArray = JSON.parse(localStorage.getItem("messagesStored")) || [];
-let list = document.getElementById("list");
+let form = document.getElementById("userInputForm");
 
-/* ======================= LOAD MESSAGES WHEN PAGE FINISH LOADING ======================= */
-
-document.addEventListener("DOMContentLoaded", function MessagesLoad() {
-  let parsedMessages = JSON.parse(localStorage.getItem("messagesStored"));
-  parsedMessages.forEach((element) => {
-    var entry = document.createElement("li");
-    entry.appendChild(document.createTextNode(element));
-    list.appendChild(entry);
-  });
-});
-
-/* ======================= SAVE MESSAGE =================================== */
-
-form.addEventListener("submit", save);
-
-function save(event) {
-
-  // ========================== HTML RENDER =====================
-
-  // Store in variable the value of input (user message)
-  let message = form.children[1].value;
-
-  // Prevent the reload on save button
-  event.preventDefault();
-
-  // Modularize the createElement
-  var entry = document.createElement("li");
-
-  // create TextNode into the new LI, with the message the user entered.
-  entry.appendChild(document.createTextNode(message));
-
-  // append the new LI to the list
-  list.appendChild(entry);
-  
-  // =================== LOCAL STORAGE SAVE =====================
-  
-  messagesArray.push(message);
-
-  let messagesArrayString = JSON.stringify(messagesArray);
-
-  localStorage.setItem("messagesStored", messagesArrayString);
-
-  form.reset();
-}
+form.addEventListener('submit', function guardar(event) {
+    event.preventDefault();
+    let formDataElement = new FormData(event.target);
+    console.log(formDataElement.get('inputMessage'))
+})
